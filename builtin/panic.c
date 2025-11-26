12966a844dab5770c3577ef3db7874e4b0b3fb1e
Captainexpo-1 <captainexpo@gmail.com>
2025-11-26 03:07:54 -0500

    added runtime to cmake

--- builtin/panic.c
+++ builtin/panic.c
@@ -8,40 +8,29 @@ int __strlen(const char *s) {
   }
   return len;
 }
-
+void __printint(int value) {
+  char buf[20];
+  int len = 0;
+  int temp = value;
+  if (temp == 0) {
+    buf[len++] = '0';
+  } else {
+    while (temp > 0) {
+      buf[len++] = '0' + (temp % 10);
+      temp /= 10;
+    }
+  }
+  for (int i = len - 1; i >= 0; i--) {
+    write(2, &buf[i], 1);
+  }
+}
 void __panic__(const char *msg, int line, int col) {
   write(2, "PANIC: ", 7);
   write(2, msg, __strlen(msg));
   write(2, " at line ", 9);
-  char line_buf[12];
-  int line_len = 0;
-  int temp_line = line;
-  if (temp_line == 0) {
-    line_buf[line_len++] = '0';
-  } else {
-    while (temp_line > 0) {
-      line_buf[line_len++] = '0' + (temp_line % 10);
-      temp_line /= 10;
-    }
-  }
-  for (int i = line_len - 1; i >= 0; i--) {
-    write(2, &line_buf[i], 1);
-  }
+  __printint(line);
   write(2, ", col ", 6);
-  char col_buf[12];
-  int col_len = 0;
-  int temp_col = col;
-  if (temp_col == 0) {
-    col_buf[col_len++] = '0';
-  } else {
-    while (temp_col > 0) {
-      col_buf[col_len++] = '0' + (temp_col % 10);
-      temp_col /= 10;
-    }
-  }
-  for (int i = col_len - 1; i >= 0; i--) {
-    write(2, &col_buf[i], 1);
-  }
+  __printint(col);
   write(2, "\n", 1);
   abort();
 }
